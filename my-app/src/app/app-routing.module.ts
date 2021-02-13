d72086054714cbb5954bf413b2e2c7d2ecab5dba
Ajay Arumugam <ajayarumugam07@gmail.com>
2021-02-13 13:42:31 -0600

    basic template ready

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { HomeComponent } from './home/home.component';
+import { SocialComponent } from './social/social.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'social', component: SocialComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
