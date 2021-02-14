e428acc2108f7e2e66256b0c527215d67daaccff
AlanAyy <alan.alcoceriturriza@gmail.com>
2021-02-14 11:04:20 -0700

    To-Do list works

--- my-app/src/app/todo/todo.component.ts
+++ my-app/src/app/todo/todo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { FirebaseDBService } from '../firebase-db.service';
 
 @Component({
   selector: 'app-todo',
@@ -10,9 +12,52 @@ export class TodoComponent implements OnInit {
   public items: any[] = []; 
   public newTask: string;
 
-  constructor() { }
+  threads: {
+    [key: string]: {
+      firstName: string;
+      lastName: string;
+      messages: {
+        [key: string]: {
+          text: string;
+          time: string;
+          sender: string;
+        }
+      }
+    }
+  };
 
-  ngOnInit(): void { }
+  events: any[] = [];
+
+  suggestedFriendsList: string[];
+  userList: any;
+
+  constructor(public dbService: FirebaseDBService, public authService: AuthService) {
+    this.dbService.getThreads();
+    this.dbService.getFriendsList();
+    this.dbService.threads.subscribe(threads => {
+      this.threads = threads;
+    });
+    this.dbService.suggestedFriendsList.subscribe(resData => {
+      console.log(resData);
+      this.suggestedFriendsList = resData;
+    });
+    this.dbService.userList.subscribe(resData => {
+      console.log(resData);
+      this.userList = resData;
+    });
+  }
+
+  ngOnInit(): void {
+    this.dbService.getCalendar().subscribe((resData:any) => {
+      for(let key of Object.keys(resData)) {
+        let date = new Date(resData[key].end);
+        let today = new Date();
+        if(date.getDate() == today.getDate() && date.getMonth == today.getMonth && date.getFullYear == today.getFullYear) {
+          this.events.push(resData[key]);
+        }
+      }
+    });
+  }
 
   public addToList() { 
     if (this.newTask == '') { 
